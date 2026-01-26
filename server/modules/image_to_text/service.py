4d2629e9efea416ec0a8b7eb27cd56756ed4a4c5
Rival Arya <rifall.aryap@gmail.com>
2026-01-26 17:19:24 +0700

    feat: make extract image more accurate with pre-processing

--- server/modules/image_to_text/service.py
+++ server/modules/image_to_text/service.py
@@ -1,12 +1,30 @@
+import cv2
+import numpy as np
 import pytesseract
 from PIL import Image
 from io import BytesIO
 
-import os
-pytesseract.pytesseract.tesseract_cmd = os.path.join(os.path.dirname(__file__), 'Tesseract-OCR', 'tesseract.exe')
-# pytesseract.pytesseract.tesseract_cmd = r"C:\Users\ThinkPad\Downloads\code\image-to-text\server\modules\image_to_text\Tesseract-OCR\tesseract.exe"
-
 def extract_text_from_image(image_bytes: bytes) -> str:
-    image = Image.open(BytesIO(image_bytes))
-    text = pytesseract.image_to_string(image)
+    image = Image.open(BytesIO(image_bytes)).convert("RGB")
+    img = np.array(image)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    gray = cv2.fastNlMeansDenoising(gray, h=30)
+    thresh = cv2.adaptiveThreshold(
+        gray,
+        255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        31,
+        2
+    )
+    config = r"--oem 3 --psm 6"
+
+    text = pytesseract.image_to_string(
+        thresh,
+        lang="eng",
+        config=config
+    )
+
     return text.strip()
